e33ad65ff398a74d8c05c3ed680dd22a83c5e920
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2025-10-09 14:37:06 +1000

    test layout

--- app/src/main/java/com/sijan003/tanvir003_book/MainActivity.kt
+++ app/src/main/java/com/sijan003/tanvir003_book/MainActivity.kt
@@ -1,34 +1,82 @@
 package com.sijan003.tanvir003_book
 
+import android.R.attr.name
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sijan003.tanvir003_book.image_ids
 import com.sijan003.tanvir003_book.ui.theme.Sijan003_BookTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             Sijan003_BookTheme {
+                //Scaffold: Layout component for material design layout
+                //top app bar, bottom bar, drawer
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    //Greeting(
+                      //  name = "Android",
+                        //modifier = Modifier.padding(innerPadding)
+                    //)
+                    TestLayout(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
+
+@Composable
+fun TestLayout(modifier: Modifier= Modifier)
+{
+    Row (modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxHeight().width(100.dp).background(Color.Green),
+            verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+          repeat(6){
+              //display a dice
+              Image(
+                  painter = painterResource(image_ids[it]),
+                  contentDescription = "Dice $it")
+          }
+        }
+        Column(modifier = Modifier.fillMaxHeight().width(100.dp).background(Color.Gray),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+            Text(text = "Column 2")
+
+        }
+        Column(modifier = Modifier.fillMaxHeight().fillMaxWidth().background(Color.Cyan),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+            Text(text = "Column 3")
+        }
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -45,3 +93,13 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+private val image_ids=listOf(
+    R.drawable.dice_1,
+    R.drawable.dice_2,
+    R.drawable.dice_3,
+    R.drawable.dice_4,
+    R.drawable.dice_5,
+    R.drawable.dice_6
+
+)
